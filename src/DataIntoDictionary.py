50b3f6feacdc6a558bcf794f2558d2ed34cfbfcb
Nando <fernandocalva15@gmail.com>
2026-05-29 00:31:46 -0600

    Solucion de problemas con la importacion en la primera linea resulta, diccionario cambiado para mejor uso y Lista csv de ejemplos para envios

--- src/DataIntoDictionary.py
+++ src/DataIntoDictionary.py
@@ -1,4 +1,4 @@
-from ExtractData_File import ExtractData_File
+from src.ExtractData_File import ExtractData_File
 
 class DataIntoDictionary:
     def __init__(self):
@@ -7,14 +7,14 @@ class DataIntoDictionary:
     def convert_to_dictionary(self):
         Directory = ExtractData_File(self.file_path).extract_data()
         ListaCompleta = []
-        dictionary = {}
         keys = Directory[0].split(",")
         for line in Directory[1:]:
             values = line.split(",")
+            dictionary = {}
             for key, value in zip(keys, values):
                 dictionary[key] = value
             ListaCompleta.append(dictionary)
         return ListaCompleta
 
-data = DataIntoDictionary()
-print(data.convert_to_dictionary())
+#data = DataIntoDictionary()
+#print(data.convert_to_dictionary())
